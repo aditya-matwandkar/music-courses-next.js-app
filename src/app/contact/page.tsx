b88e56bbaa7313ebceda39d2a3c03767431aac4e
Aditya Matwandkar <amatwandkar@gmail.com>
2024-03-03 13:48:51 +0530

    second commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { FormEvent, useState } from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+function page() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Submitted:", { email, message });
+  };
+
+  return (
+    <div className="h-[40rem] sm:h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
+      <div className="max-w-2xl mx-auto p-4 relative z-10">
+        <h1 className="text-3xl md:text-7xl text-center font-sans font-bold mb-8 text-[#F5F5F5]">
+          Contact Us
+        </h1>
+        <p className="text-neutral-400 max-w-lg mx-auto my-2 text-sm text-center">
+          We're here to help with any questions about our courses, programs, or
+          events. Reach out and let us know how we can assist you in your
+          musical journey.
+        </p>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4 mt-4">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email address"
+            className="rounded-lg border border-none focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-500"
+            required
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Your message"
+            className="rounded-lg border border-none focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-500"
+            rows={5}
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="w-fit self-end px-6 py-2 text-xs md:text-sm rounded-lg bg-teal-600 text-[#F5F5F5] font-medium hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
+
+export default page;
